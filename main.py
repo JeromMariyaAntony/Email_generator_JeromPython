50c3e4be0c4a5c36a34da311814a7697dadf566e
JeromMariyaAntony <jeromdhoni77@gmail.com>
2026-07-28 01:42:56 +0530

    main.py

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
+#Written by Jerom Mariya Antony
 
+import smtplib
+import datetime as dt
+from tkinter.font import names
 
-from datetime import datetime
 import pandas
 import random
-import smtplib
-import os
+MY_EMAIL="jeromdhoni77@gmail.com"
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+SERVER="smtp.gmail.com"
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+DOB=dt.datetime(year=2026,month=7,day=28)
+curr_date=dt.datetime.now()
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+PWD = "lisj uhrt axcq fepy"
+
+birthdaynames=pandas.read_csv("birthdays.csv")
+
+dicbirthname=birthdaynames.to_dict(orient="records")
+# print(dicbirthname)
+for i in dicbirthname:
+    # print(i)
+    names=i["name"]
+    year=i["year"]
+    month=i["month"]
+    day=i["day"]
+    if month==curr_date.month and day==curr_date.day:
+        replaced_name=names
+
+# print(replaced_name)
+randomin=random.randint(1,3)
+letterrandom=f"letter_{randomin}.txt"
+with open(f"letter_templates/{letterrandom}","r") as letter1:
+    firstletter=letter1.read()
+    final_letter= firstletter.replace("[NAME]",replaced_name)
+
+    with smtplib.SMTP(SERVER) as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+        connection.login(user=MY_EMAIL, password=PWD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs="1331jero@gmail.com",
+                            msg=f"Subject:Happy Birthday\n\n {final_letter}")
+
+
+
